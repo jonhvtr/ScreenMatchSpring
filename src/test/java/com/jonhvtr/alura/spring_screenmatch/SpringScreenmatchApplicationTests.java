af8102963f33118919749dd0ee8796db67a66a14
jonhvtr <jonhvtr_dev@hotmail.com>
2025-05-26 16:17:06 -0300

    first commit

---
+++ src/test/java/com/jonhvtr/alura/spring_screenmatch/SpringScreenmatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jonhvtr.alura.spring_screenmatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringScreenmatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
