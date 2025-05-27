a80093a6f215e6c39dc33883adc706dffc188833
jonhvtr <jonhvtr_dev@hotmail.com>
2025-05-27 18:26:15 -0300

    refactor: isolates initial configuration in separate class

--- src/main/java/com/jonhvtr/alura/spring_screenmatch/SpringScreenmatchApplication.java
+++ src/main/java/com/jonhvtr/alura/spring_screenmatch/SpringScreenmatchApplication.java
@@ -1,9 +1,6 @@
 package com.jonhvtr.alura.spring_screenmatch;
 
-import com.jonhvtr.alura.spring_screenmatch.model.DataSerie;
-import com.jonhvtr.alura.spring_screenmatch.service.ConsumingData;
-import com.jonhvtr.alura.spring_screenmatch.service.ConvertingData;
-import io.github.cdimascio.dotenv.Dotenv;
+import com.jonhvtr.alura.spring_screenmatch.principal.Principal;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -11,20 +8,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringScreenmatchApplication implements CommandLineRunner {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringScreenmatchApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringScreenmatchApplication.class, args);
+    }
 
-	@Override
-	public void run(String... args) throws Exception {
-		ConsumingData consumingData = new ConsumingData();
-		Dotenv dotenv = Dotenv.load();
-		String apiKey = dotenv.get("API_KEY");
-		var json = consumingData.getData("http://www.omdbapi.com/?t=gilmore+girls" +
-				"&apikey=" + apiKey);
-		System.out.println(json);
-		ConvertingData convertingData = new ConvertingData();
-		DataSerie data = convertingData.getData(json, DataSerie.class);
-		System.out.println(data);
-	}
+    @Override
+    public void run(String... args) throws Exception {
+        Principal principal = new Principal();
+        principal.exibeMenu();
+    }
 }
