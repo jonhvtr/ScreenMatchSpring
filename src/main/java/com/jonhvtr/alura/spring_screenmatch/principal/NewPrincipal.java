4144b51ef0402c8fc019dfcbbc14343b53f3e289
jonhvtr <jonhvtr_dev@hotmail.com>
2025-07-16 16:30:04 -0300

    feat: search for information in the database

--- src/main/java/com/jonhvtr/alura/spring_screenmatch/principal/NewPrincipal.java
+++ src/main/java/com/jonhvtr/alura/spring_screenmatch/principal/NewPrincipal.java
@@ -1,9 +1,6 @@
 package com.jonhvtr.alura.spring_screenmatch.principal;
 
-import com.jonhvtr.alura.spring_screenmatch.model.DataSeason;
-import com.jonhvtr.alura.spring_screenmatch.model.DataSerie;
-import com.jonhvtr.alura.spring_screenmatch.model.Episode;
-import com.jonhvtr.alura.spring_screenmatch.model.Serie;
+import com.jonhvtr.alura.spring_screenmatch.model.*;
 import com.jonhvtr.alura.spring_screenmatch.repository.SerieRepository;
 import com.jonhvtr.alura.spring_screenmatch.service.ConsumingData;
 import com.jonhvtr.alura.spring_screenmatch.service.ConvertingData;
@@ -38,6 +35,11 @@ public class NewPrincipal {
                     1 - Buscar séries
                     2 - Buscar episódios
                     3 - Buscar séries buscadas
+                    4 - Buscar por título
+                    5 - Buscar séries por autor
+                    6 - Top 5 Séries
+                    7 - Buscar Série por Categoria
+                    8 - Buscar Séries por temporada
                     
                     0 - Sair
                     """;
@@ -55,6 +57,21 @@ public class NewPrincipal {
                     break;
                 case 3:
                     searchHistory();
+                    break;
+                case 4:
+                    searchSerieByTitle();
+                    break;
+                case 5:
+                    searchSerieByActors();
+                    break;
+                case 6:
+                    searchTop5Serie();
+                    break;
+                case 7:
+                    searchSeriesByCategory();
+                    break;
+                case 8:
+                    searchSeriesByNumberOfSeasons();
                     break;
                 case 0:
                     System.out.println("Saindo...");
@@ -85,9 +102,7 @@ public class NewPrincipal {
         System.out.println("Escolha uma série:");
         var nameSerie = scan.nextLine();
 
-        Optional<Serie> serie = series.stream()
-                .filter(s -> s.getTitle().toLowerCase().contains(nameSerie.toLowerCase()))
-                .findFirst();
+        Optional<Serie> serie = serieRepository.findByTitleContainingIgnoreCase(nameSerie);
 
         if (serie.isPresent()) {
             var seriesFound = serie.get();
@@ -117,4 +132,54 @@ public class NewPrincipal {
         series = serieRepository.findAll();
         series.stream().sorted(Comparator.comparing(Serie::getGenre)).forEach(System.out::println);
     }
+
+    private void searchSerieByTitle() {
+        System.out.println("Escolha uma série:");
+        var nameSerie = scan.nextLine();
+
+        Optional<Serie> foundSeries = serieRepository.findByTitleContainingIgnoreCase(nameSerie);
+
+        if (foundSeries.isPresent()) {
+            System.out.println("Dados da série: " + foundSeries.get());
+        } else {
+            System.out.println("Série não encontrada");
+        }
+    }
+
+    private void searchSerieByActors() {
+        System.out.println("Qual o nome para a busca?");
+        var nameActor = scan.nextLine();
+        System.out.println("Avaliações a partir de que valor?");
+        var rating = scan.nextDouble();
+        List<Serie> foundSeries = serieRepository.findByActorsContainingIgnoreCaseAndRatingGreaterThanEqual(nameActor
+                , rating);
+        System.out.println("Séries em que " + nameActor + " trabalhou: ");
+        foundSeries.forEach(s -> System.out.println(s.getTitle() + " avaliação: " + s.getRating()));
+    }
+
+    private void searchTop5Serie() {
+        List<Serie> topSeries = serieRepository.findTop5ByOrderByRatingDesc();
+        topSeries.forEach(s -> System.out.println(s.getTitle() + " avaliação: " + s.getRating()));
+    }
+
+    private void searchSeriesByCategory() {
+        System.out.println("Escolha uma categoria:");
+        var nameCategory = scan.nextLine();
+        Category category = Category.fromString(nameCategory);
+        List<Serie> seriesByCategory = serieRepository.findByGenre(category);
+        System.out.println("Séries da Categoria" + seriesByCategory);
+        seriesByCategory.forEach(System.out::println);
+    }
+
+    private void searchSeriesByNumberOfSeasons() {
+        System.out.println("Digite o números de temporadas que deseja:");
+        var numSeasons = scan.nextInt();
+        System.out.println("Digite o número de avaliações que deseja:");
+        var rating = scan.nextDouble();
+        List<Serie> seriesByNumberOfSeasons =
+                serieRepository.findByTotalSeasonsLessThanEqualAndRatingGreaterThanEqual(numSeasons, rating);
+        System.out.println("Séries encontradas:");
+        seriesByNumberOfSeasons.forEach(System.out::println);
+    }
+
 }
--- src/main/java/com/jonhvtr/alura/spring_screenmatch/repository/SerieRepository.java
+++ src/main/java/com/jonhvtr/alura/spring_screenmatch/repository/SerieRepository.java
@@ -1,7 +1,20 @@
 package com.jonhvtr.alura.spring_screenmatch.repository;
 
+import com.jonhvtr.alura.spring_screenmatch.model.Category;
 import com.jonhvtr.alura.spring_screenmatch.model.Serie;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface SerieRepository extends JpaRepository<Serie, Long> {
+    Optional<Serie> findByTitleContainingIgnoreCase(String nameSerie);
+
+    List<Serie> findByActorsContainingIgnoreCaseAndRatingGreaterThanEqual(String nameActor, double rating);
+
+    List<Serie> findTop5ByOrderByRatingDesc();
+
+    List<Serie> findByGenre(Category category);
+
+    List<Serie> findByTotalSeasonsLessThanEqualAndRatingGreaterThanEqual(Integer totalSeasons, double rating);
 }
